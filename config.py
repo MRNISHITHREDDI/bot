d6b974e454c16953158420a66d65948ae13ccd61
SAINISHITHREDDY <89187213+MRNISHITHREDDI@users.noreply.github.com>
2025-03-14 22:58:22 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+BOT_TOKENS = [
+    "7969285298:AAHuohmfq7rVhJiIymW5x2B6TeF-b8VnxCM",
+    "8075551315:AAHWTIU-ZOfzbm-LlFuawoEiSwWC72RU3EA",
+    "7776558825:AAGBhnpwwGwaBXQ22YZxGLKshNE3_nqLfJ8",
+    "7632399315:AAEwJU2PpSG-6pbbj4cKbrFwlmBP8FJYlww",
+    "7880551516:AAGIN9b57Q18DL5936sd-3xpf79kZNFGc0U"
+]
+
+BOT_EMOJIS = ["🤩", "🤩", "🔥", "🏆", "❤️", "👍"]
+
+IGNORED_MESSAGES = [
+    "⚠️ chart not stable 🥹 wait for 2 minutes 🤩",
+    "⚠️ chart not stable 🥹 wait 2 minutes 🤩"
+]
